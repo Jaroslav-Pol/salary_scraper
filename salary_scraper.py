b262aa733092553840bd712af6c2f1f5c3c3c095
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2021-12-16 22:25:43 +0200

    lAT Pge function was incorrect, found error(driver.quit), now everything is working for test. Page number just from head

--- salary_scraper.py
+++ salary_scraper.py
@@ -21,44 +21,49 @@ result_dic = {'position': [],
               }
 
 
-def last_page():
-    """returns last page of the current search (in cvbankas.lt)"""
-    driver.get(base_url + '1')  # adds page number
+# def last_page():
+#     """returns last page of the current search (in cvbankas.lt)"""
+#     driver.get(base_url + '1')  # adds page number
+#     content = driver.page_source
+#     soup = BeautifulSoup(content)
+#     driver.quit()
+#     html_elements = soup.find(class_='pages_ul_inner').find_all('a')
+#     return int(html_elements[-1].text)
+# print(last_page())
+
+
+for page in range(12):
+    time.sleep(2)
+    new_url = base_url + str(page + 1)
+    print(new_url)
+
+    driver.get(new_url)
     content = driver.page_source
     soup = BeautifulSoup(content)
-    driver.quit()
-    html_elements = soup.find(class_='pages_ul_inner').find_all('a')
-    return int(html_elements[-1].text)
+    # driver.quit()
 
+    for a in soup.find_all(class_='list_a_wrapper'):
+        try:
+            position = a.find(class_='list_h3').text
+            company = a.find(class_='dib mt5').text
+            salary_range = a.find(class_='salary_amount').text
+            if a.find(class_='salary_calculation').text == 'Neatskaičius mokesčių':
+                bruto = True
+            else:
+                bruto = False
+            results.append([position, company, salary_range, bruto])
+        except:
+            content
 
-# for page in range(last_page()):
-#     time.sleep(3)
-#     new_url = base_url + str(page + 1)
-#     print(new_url)
+    print(results)
 
-driver.get(base_url + '1')
-content = driver.page_source
-soup = BeautifulSoup(content)
+    for element in results:
+        result_dic['position'].append(element[0])
+        result_dic['company'].append(element[1])
+        result_dic['salary range'].append(element[2])
+        result_dic['bruto'].append(element[3])
+
 driver.quit()
-
-for a in soup.find_all(class_='list_a_wrapper'):
-    position = a.find(class_='list_h3').text
-    company = a.find(class_='dib mt5').text
-    salary_range = a.find(class_='salary_amount').text
-    if a.find(class_='salary_calculation').text == 'Neatskaičius mokesčių':
-        bruto = True
-    else:
-        bruto = False
-    results.append([position, company, salary_range, bruto])
-
-print(results)
-
-for element in results:
-    result_dic['position'].append(element[0])
-    result_dic['company'].append(element[1])
-    result_dic['salary range'].append(element[2])
-    result_dic['bruto'].append(element[3])
-
 print(result_dic)
 df = pd.DataFrame(result_dic)
 df.to_csv('salaries2.csv', index=False, encoding='utf-8')
